a3348485884159c7f391ea20754ef4fb19186bbf
jaguarshadow <starbucknakari@gmail.com>
2023-07-21 18:26:49 -0500

    exercise 3.8 phonebook

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+var morgan = require('morgan')
+
 const app = express()
 
 app.use(express.json())
+morgan.token('body', function(req, res) { return JSON.stringify(req.body) })
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms - :body'))
 
 let persons = [
     { 
@@ -37,8 +41,8 @@ app.get('/api/persons', (request, response) => {
 app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     const person = persons.find(p => p.id === id)
-    if (person) response.json(person)
-    else response.status(404).end()
+    if (person) return response.json(person)
+    return response.status(404).end()
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -76,6 +80,12 @@ app.get('/info', (request, response) => {
     <p>${date}</p>
     `)
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT)
